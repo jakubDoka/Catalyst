84d1f614359f2238aa8335263e42bf416988d988
mlokis <jakub.doka2@gmail.com>
2022-07-04 17:56:44 +0200

    cleanup

--- ownership_types/src/error.rs
+++
@@ -1,8 +0,0 @@
-use lexer::Span;
-
-pub enum OwError {
-    DoubleMove { because: Span, loc: Span },
-    MoveFromBehindPointer { loc: Span },
-    LoopDoubleMove { because: Span, loc: Span },
-    PartiallyMovedDrop { because: Span, loc: Span },
-}
